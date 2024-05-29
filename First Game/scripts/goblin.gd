053688a3cdd438bdce23850ee18a5ace050fb2d8
Lisa Bacherle <ldbacherle@gmail.com>
2024-05-29 20:14:06 +0200

    #1: add new enemy type and collision rays

--- First Game/scripts/goblin.gd
+++ First Game/scripts/goblin.gd
@@ -1,31 +1,24 @@
 extends Node2D
 
-const SPEED = 60
+const SPEED = 30
 
 var direction = 1
 
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite = $AnimatedSprite2D
-@onready var ray_cast_down_left = $RayCastDownLeft
-@onready var ray_cast_down_right = $RayCastDownRight
+
+
 # Ray cast nach unten????
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ray_cast_right.is_colliding():
 		direction = -1
-		animated_sprite.flip_h = true
-	elif ray_cast_left.is_colliding():
-		direction = 1
 		animated_sprite.flip_h = false
-	elif not ray_cast_down_left.is_colliding():
-		print("left not colliding")
+	if ray_cast_left.is_colliding():
 		direction = 1
-		animated_sprite.flip_h = false
-	elif not ray_cast_down_right.is_colliding():
-		print("right not colliding")
-		direction = -1
 		animated_sprite.flip_h = true
+		
 	
 	position.x += direction * SPEED * delta
